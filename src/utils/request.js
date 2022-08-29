b89f19828443cb5e9138dff2f89e12c6515a7be9
Tatsumi <xchwei@qq.com>
2022-08-29 14:35:06 +0800

    添加响应拦截器
    处理请求返回数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,4 +4,14 @@ const request = axios.create({
   baseURL: "http://geek.itheima.net/",
 })
 
+//添加响应拦截器
+request.interceptors.response.use(
+  res => {
+    return res.data
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
 export default request
